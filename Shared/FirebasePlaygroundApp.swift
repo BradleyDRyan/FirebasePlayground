81edbd6d0f94b0b172e21540bfa7488b20972a33
Bradley Ryan <52720930+BradleyDRyan@users.noreply.github.com>
2022-03-21 15:15:36 -0600

    Initial Commit

---
+++ Shared/FirebasePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePlaygroundApp.swift
+//  Shared
+//
+//  Created by Bradley Ryan on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
